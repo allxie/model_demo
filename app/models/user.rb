7f701e877fd021ed1d12d67af78073db2e1d50ea
Allxie <allxiecleary@gmail.com>
2015-04-26 16:56:53 -0700

    demos models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+	# Validates all of those things
+	validates :email, :password, :first_name, :last_name, presence: true
+	validates :age, numericality: { greater_than_or_equal_to: 0, only_integer: true}
+
+	after_commit :send_email
+
+
+	
+	def send_email
+		#send welcome email
+		puts "Welcome email sent successfully."
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424180331) do
+ActiveRecord::Schema.define(version: 20150424183325) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150424180331) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
